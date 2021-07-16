89eb83e5bf35325026f4ff1ee299b3760cde90bd
Alexander "Ace" Olofsson <ace@haxalot.com>
2021-07-16 15:54:27 +0200

    Update example knotify script

--- knotification-script.example.sh
+++ knotification-script.example.sh
@@ -3,17 +3,12 @@
 # Ensure everything else gets a chance to complete before the script triggers
 sleep 0.1
 
-# Avoid acting on deactivations
-tail -n1 /tmp/network-connections.log | grep 'deactivated' &> /dev/null && return 0
-
-# Requires KNotification with the network event "Connection Established" to be
-# set up to log to file at /tmp/network-connections.log and run this script
-NETWORK=$(tail -n1 /tmp/network-connections.log | sed "s/.*'\(.*\)'.*/\1/")
-
-echo "Connected to \"$NETWORK\" at $(date)" >> /tmp/autologin.log
-
 # Need phantomjs on the path
 export PATH="~/.bin:$PATH"
-wifi-autologin --login "$NETWORK" &>> /tmp/autologin.log
-
-echo >> /tmp/autologin.log
+if which systemd-cat &> /dev/null; then
+    wifi-autologin --login --auto |& systemd-cat -t wifi-autologin
+elif which logger &> /dev/null; then
+    wifi-autologin --login --auto |& logger -t wifi-autologin
+else
+    wifi-autologin --login --auto >>& /tmp/autologin.log
+fi
