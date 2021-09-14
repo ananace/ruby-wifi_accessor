530d4d277987cb0f74c73b6b84bda920097b5cbf
Alexander "Ace" Olofsson <ace@haxalot.com>
2021-09-14 07:33:29 +0200

    Avoid overwriting manually specified url

--- lib/wifi_accessor/network.rb
+++ lib/wifi_accessor/network.rb
@@ -2,9 +2,8 @@
 
 module WifiAccessor
   class Network
-    attr_accessor :name, :login, :data
+    attr_accessor :name, :login, :data, :url
     attr_reader :hooks
-    attr_writer :url
 
     def initialize(name:, login:, **params)
       @name = name
